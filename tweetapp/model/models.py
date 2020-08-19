2430198ebf24f21aa7f8e73d0d37a59ec77f90cd
damianra <44167699+damianra@users.noreply.github.com>
2020-08-19 10:45:38 -0300

    Update models.py

--- tweetapp/model/models.py
+++ tweetapp/model/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class SearchHistory(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     search_text = models.CharField(max_length=150)
     date = models.CharField(max_length=150)
 
